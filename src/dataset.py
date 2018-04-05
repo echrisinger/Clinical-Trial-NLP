9ef45fdc96dfac941a286f891dcc1cdba6118053
Lee Norgaard <leemnorgaard@gmail.com>
2018-04-05 15:49:09 -0700

    added functions to plot selection critia met/not met counts

--- src/dataset.py
+++ src/dataset.py
@@ -2,14 +2,16 @@ import xml.etree.ElementTree as ET
 import argparse
 from os import walk
 import numpy as np
+import matplotlib.pyplot as plt
 import config
+
+column_titles = ["ABDOMINAL", "ADVANCED-CAD", "ALCOHOL-ABUSE", "ASP-FOR-MI", "CREATININE", "DIETSUPP-2MOS", "DRUG-ABUSE", "ENGLISH", "HBA1C", "KETO-1YR", "MAJOR-DIABETES", "MAKES-DECISIONS", "MI-6MOS"]
 
 # Get the string paths of each file at the final directory of the path
 def get_files(path='../res/train'):
     files = []
     for (dirpath, dirnames, filenames) in walk(path, topdown=False):
-        print filenames
-        # TODO: should have train_path at start
+        # print filenames
         files.extend([dirpath+'/'+name for name in filenames])
         break
     return files
@@ -43,6 +45,74 @@ def get_labels(files):
         y[i] = y_f
     return y
 
+
+# Count up the number of met/not met for each selection criterion
+def met_not_met_counts(y):
+    """
+    Inputs:
+    y -- the labels, a (202, 13) array
+
+    Outputs:
+    label_sums -- (13, 2) array, each row is a selection criterion,
+                    column 0 is "not met" counts, column 1 is "met" counts
+    """
+    label_sums = np.zeros((13, 2))
+    for labels in y:
+        for j in range(13):
+            if labels[j] < 0:
+                label_sums[j][0] = label_sums[j][0] + labels[j]
+            if labels[j] > 0:
+                label_sums[j][1] = label_sums[j][1] + labels[j]
+    return label_sums
+
+
+# Plot the number of met/not met for each selection criterion
+def visualize_original_labels(label_sums):
+    """
+    Inputs:
+    label_sums -- (13, 2) array, each row is a selection criterion,
+                    column 0 is "not met" counts, column 1 is "met" counts
+    Outputs:
+    None (a bar chart)
+    """
+    met = []
+    not_met = []
+    for i in label_sums:
+        met.append(i[1])
+        not_met.append(i[0] * -1)
+
+    N = 13
+    ind = np.arange(N)  # the x locations for the groups
+    width = 0.35       # the width of the bars
+
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(ind, met, width, color='y')
+    rects2 = ax.bar(ind + width, not_met, width, color='b')
+
+    # add some text for labels, title and axes ticks
+    ax.set_ylabel('Count')
+    ax.set_title('Met/Not Met Counts By Selection Criteria')
+    ax.set_xticks(ind + width / 2)
+    ax.set_xticklabels(column_titles)
+    ax.legend((rects1[0], rects2[0]), ('Met', 'Not Met'))
+
+    def autolabel(rects):
+        """
+        Attach a text label above each bar displaying its height
+        """
+        for rect in rects:
+            height = rect.get_height()
+            ax.text(rect.get_x() + rect.get_width()/2., height,
+                    '%d' % int(height),
+                    ha='center', va='bottom')
+
+    autolabel(rects1)
+    autolabel(rects2)
+    plt.xticks(rotation=90)
+    plt.show()
+
+
+
 def main():
     parser = argparse.ArgumentParser(description='Parse dataset params')
     parser.add_argument('train_path', type=str, help='path to the training data directory locally')
@@ -53,6 +123,11 @@ def main():
 
     X = get_raw_words(files)
     y = get_labels(files)
+
+    # Bar chart of met/not met counts for each selection criterion
+    label_sums = met_not_met_counts(y)
+    visualize_original_labels(label_sums)
+
 
 if __name__ == "__main__":
     main()
