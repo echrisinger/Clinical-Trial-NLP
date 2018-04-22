82c8bed3e03fc40808cc0de9a9ba16874f7db15a
Evan Chrisinger <echrisinger@gmail.com>
2018-04-22 16:22:46 -0700

    get tf's for positive examples

--- src/learning.py
+++ src/learning.py
@@ -7,6 +7,7 @@ from dataset import get_Xy
 from nltk.stem import *
 
 from utils import stem_tokens
+import config
 
 def tfidf_tokenizer(text):
     tokens = nltk.word_tokenize(text)
@@ -17,8 +18,17 @@ def tfidf_tokenizer(text):
 def apply_tfidf():
     pfiles, y = get_Xy()
     tfidf = TfidfVectorizer(tokenizer=tfidf_tokenizer, stop_words='english')
-    tfs = tfidf.fit_transform([pfiles[0].entries[0].text])
+    for label in range(config.NUM_LABELS):
+        pos_pfiles, pos_y = zip(*[(pfile, y[i]) for i, pfile in enumerate(pfiles) if y[i][label] == 1])
+        neg_pfiles, neg_y = zip(*[(pfile, y[i]) for i, pfile in enumerate(pfiles) if y[i][label] == -1])
+        
+        pos_text = [entry.text for entry in pfile.entries for pfile in pos_pfiles]
+        pos_tfs = tfidf.fit_transform(pos_text)
 
+        neg_text = [entry.text for entry in pfile.entries for pfile in neg_pfiles]
+        neg_tfs = tfidf.fit_transform(neg_text)
+
+        
 def main():
     apply_tfidf()
 
