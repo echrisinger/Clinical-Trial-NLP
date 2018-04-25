21f7e2f6da113f2a550a1feb4d4197aa61698d8d
Evan Chrisinger <echrisinger@gmail.com>
2018-04-24 20:50:26 -0700

    evan changes

--- src/learning.py
+++ src/learning.py
@@ -4,9 +4,13 @@ nltk.download('punkt')
 nltk.download('stopwords')
 
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from dataset import get_Xy, parse_args
+from dataset import get_Xy, parse_args, column_titles
 from nltk.stem import *
+from sklearn.metrics import confusion_matrix, accuracy_score, f1_score, precision_score, recall_score, roc_auc_score
+
+
 from sklearn.svm import SVC
 
 from utils import stem_tokens, concat_entries
@@ -44,6 +48,8 @@ def main():
     pfiles, y = get_Xy(args.train_path)
     X_train, X_test, y_train, y_test = train_test_split(pfiles, y, test_size=0.1)
 
+    label_scores = [{} for _ in range(config.NUM_LABELS)]
+
     for label in range(args.n_labels):
         
         tfidf = TfidfVectorizer(tokenizer=tfidf_tokenizer, stop_words='english')
@@ -65,19 +71,47 @@ def main():
         tfidf.fit(cat_pfiles)
         tfs = tfidf.transform(cat_train_files).toarray()
 
-        clf = SVC(C=1.0, kernel='rbf')
-        clf.fit(tfs, labeled_y)
+        c_range = 10.0 ** np.arange(-3, 3)
+        c_scores = [0 for _ in c_range]
         
-        test_labels = [y_test[i][label] for i in range(len(y_test))]
-        test_tfs    = tfidf.transform(cat_test_files).toarray()
+        f_svc = open('svc_results.txt', "w+")
+        f_svc.write('label #: {}'.format(column_titles[label]))
+        for i, c_num in enumerate(c_range):
+            clf = SVC(C=c_num, kernel='rbf')
+            clf.fit(tfs, labeled_y)
+            
+            test_labels = [y_test[k][label] for k in range(len(y_test))]
+            test_tfs    = tfidf.transform(cat_test_files).toarray()
 
-        for i in range(len(test_labels)):
-            if test_labels[i] < 0:
-                test_labels[i] = 0
-                
-        acc = clf.score(test_tfs, test_labels)
-        print 'label {} accuracy is {}'.format(label, acc)
+            for j in range(len(test_labels)):
+                if test_labels[j] < 0:
+                    test_labels[j] = 0
+                    
+            results = clf.predict(test_tfs)
 
+            score = {}
+            score['C'] = c_num
+            score['accuracy'] = clf.score(test_tfs, test_labels)
+            score['f1']  = f1_score(test_labels, results)
+            score['auroc'] = roc_auc_score(test_labels, results, average="macro")
+            score['precision'] = precision_score(test_labels, results)
+            score['sensitivity'] = recall_score(test_labels, results)
+            score['specificity'] = (lambda tn, fp, fn, tp: tn / float(tn+fp))(*(confusion_matrix(test_labels, results).ravel()))
+            
+            f_svc.write("C: {}\n".format(c_num))
+            f_svc.write("accuracy: {}\n".format(score['accuracy']))
+            f_svc.write("f1: {}\n".format(score['f1']))
+            f_svc.write("auroc: {}\n".format(score['auroc']))
+            f_svc.write("precision: {}\n".format(score['precision']))
+            f_svc.write("sensitivity: {}\n".format(score['sensitivity']))
+            f_svc.write("specificity: {}\n\n".format(score['specificity']))
+            
+            c_scores[i] = score
+
+            print 'done with iteration'
+
+        f_svc.close()
+        label_scores[label]['svm_scores'] = c_scores
 
 if __name__ == "__main__":
     main()
